c3250f3e61813a0c1de3bb866c93af96a70f3dab
Thomas Kaczmarek <mulenatic@gmail.com>
2016-06-21 20:54:37 +0200

    Moved the complete client side code into a separate folder.

---
+++ client/app/components/transactionSummary/transactionSummary.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "transactionSummary",
+    templateUrl: "app/components/transactionSummary/transactionSummary.html"
+})
+export class TransactionSummaryComponent {
+
+
+
+}
