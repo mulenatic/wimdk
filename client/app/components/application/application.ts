53898c0dedc99631c583e8d74d8b78d7ab1f9ec4
Thomas Kaczmarek <mulenatic@gmail.com>
2016-06-22 17:43:59 +0200

    Moved the transactionTimeline in a separate component.

--- client/app/components/application/application.ts
+++ client/app/components/application/application.ts
@@ -1,10 +1,11 @@
 import {Component} from "@angular/core";
 
 import {TransactionSummaryComponent} from "../../components/transactionSummary/transactionSummary";
+import {TransactionTimelineComponent} from "../../components/transactionTimelineComponent/TransactionTimelineComponent";
 
 @Component({
     selector: "app",
-    directives: [TransactionSummaryComponent],
+    directives: [TransactionSummaryComponent, TransactionTimelineComponent],
     templateUrl: "app/components/application/application.html"
 })
 export class ApplicationComponent { }
---
+++ client/app/components/transactionTimelineComponent/TransactionTimelineComponent.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "transactionTimeline",
+    templateUrl: "app/components/transactionTimelineComponent/TransactionTimelineComponent.html"
+})
+export class TransactionTimelineComponent { }
+
+
