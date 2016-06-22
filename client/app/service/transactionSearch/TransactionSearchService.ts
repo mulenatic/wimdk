c9f233f2af88c383c19c37a36ae93ac8f79a1a87
Thomas Kaczmarek <mulenatic@gmail.com>
2016-06-22 21:21:56 +0200

    In the transactionTimline additional content is now shown.
    In the transactionTimeline each transaction now also shows the data,
    which is stored in the sonstiges array. Here, a user will be able to put
    additional data, which does not fit into the other fields of the transaction.

--- client/app/service/transactionSearch/TransactionSearchService.ts
+++ client/app/service/transactionSearch/TransactionSearchService.ts
@@ -1,15 +1,36 @@
+export class SonstigesPair {
+
+    constructor(
+        private _label: string,
+        private _value: string
+    ) { }
+
+    get label(): string { return this._label; }
+    get value(): string { return this._value; }
+
+}
+
 export class Transaction {
 
     constructor(
-        public betrag: number,
-        public user: string,
-        public grund: string,
-        public erstellDatum: Date,
-        public bestaetigt: boolean,
-        public sonstiges: Array<any>
+        private _betrag: number,
+        private _user: string,
+        private _grund: string,
+        private _erstellDatum: Date,
+        private _bestaetigt: boolean,
+        private _sonstiges: Array<SonstigesPair>
     ) {
 
     }
+
+    get betrag(): number { return this._betrag; }
+    get user(): string { return this._user; }
+    get grund(): string { return this._grund; }
+    get erstellDatum(): Date { return this._erstellDatum; }
+    get bestaetigt(): boolean { return this._bestaetigt; }
+    get sonstiges(): Array<SonstigesPair> { return this._sonstiges; }
+
+
 
 }
 
@@ -56,12 +77,18 @@ export class TransactionSearchService {
             -50, "user1", "Monatsbeitrag Haushalt", new Date(2016, 6, 22, 17, 0, 0), false, new Array());
         result.push(transaction);
 
+
         transaction = new Transaction(
             30.99, "user2", "Bierchen in der Kneipe", new Date(2016, 6, 22, 16, 0, 0), false, new Array());
         result.push(transaction);
 
+        let sonstiges: Array<SonstigesPair> = new Array<SonstigesPair>();
+        sonstiges.push(new SonstigesPair("Ort", "Baushaus Frechen"));
+        sonstiges.push(new SonstigesPair("Farbe", "blau"));
+
+
         transaction = new Transaction(
-            -9.39, "user1", "Wasserschlauch", new Date(2016, 6, 22, 15, 0, 0), true, new Array());
+            9.39, "user1", "Wasserschlauch", new Date(2016, 6, 22, 15, 0, 0), true, sonstiges);
         result.push(transaction);
 
         return result;
